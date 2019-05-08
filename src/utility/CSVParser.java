75f58fd1b624cfb5513b3ec0256e7c228ce313a2
ambertide <egeemirozkan24@gmail.com>
2019-05-08 17:07:47 +0300

    Started

---
+++ src/utility/CSVParser.java
@@ -0,0 +1,11 @@
+package utility;
+
+import java.util.Scanner;
+
+
+public class CSVParser {
+
+	private static int countLines(Scanner fileScanner) {
+		return 1;
+		}
+}
---
+++ src/utility/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the CSVParser and Searcher classes that bridge the client and the data structure.
+ */
+package utility;
