ece1ea1947d415e823cec8e1f56f3e30f99fec1f
ambertide <egeemirozkan24@gmail.com>
2019-05-10 19:15:25 +0300

    update

--- src/IztechMediaMarketApp.java
+++ src/IztechMediaMarketApp.java
@@ -8,8 +8,8 @@ import utility.*;
 public class IztechMediaMarketApp {
 
 	public static void main(String[] args) {
-		CSVParser fileReader = new CSVParser("CENG112_HW4_Media.txt");
+/*		CSVParser fileReader = new CSVParser("CENG112_HW4_Media.txt");
 		IBinarySearchTree<IMedia> tree = fileReader.getTree();
-		UserInput.userInput(tree);
+		UserInput.userInput(tree); */
 	}
 }
